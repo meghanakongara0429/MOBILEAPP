21d2c847a16e463068c654423f741128490142cf
Meghana <meghanakongara0429@gmail.com>
2025-12-06 09:37:35 +0530

    added anchor

--- src/screens/OnboardingScreen1.jsx
+++ src/screens/OnboardingScreen1.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './ScreenStyles.css'
+import { useState } from 'react';
+
 
 const OnboardingScreen1 = ({ onNext }) => {
+    const [active, setActive] = useState(null);
     return (
-        <div className="screen">
+        <div
+            className="screen">
             <div className="status-bar">
                 <span className="time">9:37</span>
                 <div className="status-icons">
@@ -13,10 +17,27 @@ const OnboardingScreen1 = ({ onNext }) => {
                 </div>
             </div>
 
-            <div className="content-area" style={{ minHeight: '30vh' }}>
+            <div
+                className="content-area"
+            >
             </div>
 
-            <div className="purple-section">
+            <svg className="purple-arc" viewBox="0 0 400 200">
+                <path
+                    d="
+      M0 80 
+      C100 0 300 0 400 80 
+      C300 160 100 160 0 80 
+      Z
+    "
+                    fill="url(#purpleGradient)"
+                />
+            </svg>
+            <div
+                className={`purple-section ${active === "bottom" ? "highlighted" : ""}`}
+                onClick={() => setActive("bottom")}
+            >
+
                 <h1 className="headline">Talk to someone who cares about you</h1>
                 <p className="subtitle">Talk to people who understand you and don't judge</p>
 
